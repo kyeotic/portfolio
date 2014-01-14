87903473fb9bc7ddedf243ed83584a8e8f085d96
Timothy Moran <tyrsius@gmail.com>
2014-01-14 15:55:03 -0800

    working on getting r.js setup, ignor uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,81 @@
 module.exports = function(grunt){
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
-    });
+	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
-    grunt.registerTask('default', []);
+	var files = {
+		js: ['src/client/app/**/*.js', 'src/client/lib']
+	};
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		htmlhint: {
+			build: {
+				options: {
+					'tag-pair': true,
+					'tagname-lowercase': true,
+					'attr-lowercase': true,
+					'attr-value-double-quotes': true,
+					'doctype-first': true,
+					'spec-char-escape': true,
+					'id-unique': true,
+					'head-script-disabled': true,
+					'style-disabled': true
+				},
+				src: ['src/views/index.html', 'src/client/app/**/*.html']
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			build: {
+				files: {
+					'build/app.min.js': ['src/client/app/**/*.js', 'src/client/lib/durandal/**/*.js']
+				}
+			}
+		},
+		requirejs: {
+			build: {
+				options: {
+					include:['src/client/app/**/*.{js,html}', 'src/client/lib/durandal/**/*.js'],
+					loaderPluginExtensionMaps:{
+						'.html':'text'
+					},
+					rjs:{
+						name:'../lib/almond-custom', //to deploy with require.js, use the build's name here instead
+						insertRequire:['main'], //needed for almond,not require
+						baseUrl : 'src/client/app',
+						mainConfigFile:'src/client/app/main.js', //needed for almond,not require
+						wrap:true, //needed for almond,not require
+						paths : {
+							'text': '../lib/text',
+							'durandal': '../lib/durandal',
+							'plugins': '../lib/durandal/plugins',
+							'transitions': '../lib/durandal/transitions',
+							'knockout': '../lib/knockout-2.3.0',
+							'bootstrap': 'empty:',
+							'jquery': 'empty:'
+						},
+						inlineText: true,
+						optimize : 'none',
+						pragmas: {
+							build: true
+						},
+						stubModules : ['text'],
+						keepBuildDir: true,
+						out:'build/main-built.js'
+					}
+				}
+			}
+		},
+		watch: {
+			html: {
+				files: ['src/views/index.html'],
+				tasks: ['htmlhint']
+			}
+		}
+	});
+
+	grunt.registerTask('default', []);
 
 };
