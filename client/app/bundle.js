8d3d621ebaaaf44234ef99667a2bda963c4055a5
Timothy Moran <tyrsius@gmail.com>
2015-12-29 21:57:49 -0800

    adding bundle

---
+++ client/app/bundle.js
@@ -0,0 +1,9 @@
+System.register(['aurelia-bootstrapper', 'aurelia-templating-binding', 'aurelia-templating-router', 'aurelia-templating-resources', 'aurelia-event-aggregator', 'aurelia-router', 'aurelia-history', 'aurelia-history-browser', './nav-bar', './errors', './home', './navbar', './projects', './resume'], function (_export) {
+  return {
+    setters: [function (_aureliaBootstrapper) {}, function (_aureliaTemplatingBinding) {}, function (_aureliaTemplatingRouter) {}, function (_aureliaTemplatingResources) {}, function (_aureliaEventAggregator) {}, function (_aureliaRouter) {}, function (_aureliaHistory) {}, function (_aureliaHistoryBrowser) {}, function (_navBar) {}, function (_errors) {}, function (_home) {}, function (_navbar) {}, function (_projects) {}, function (_resume) {}],
+    execute: function () {
+      'use strict';
+    }
+  };
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJidW5kbGUuanMiLCJzb3VyY2VSb290IjoiLi4vc3JjLyJ9
---
+++ src/bundle.js
@@ -0,0 +1,19 @@
+// Aurelia framework
+import 'aurelia-bootstrapper';
+import 'aurelia-templating-binding';
+import 'aurelia-templating-router';
+import 'aurelia-templating-resources';
+import 'aurelia-event-aggregator';
+import 'aurelia-router';
+import 'aurelia-history';
+import 'aurelia-history-browser';
+
+// Behaviours
+import './nav-bar';
+
+// Routes/views
+import './errors';
+import './home';
+import './navbar';
+import './projects';
+import './resume';
