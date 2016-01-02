f1512704c16a0e674abbcd109ad0b1e03e36f72e
Timothy Moran <tyrsius@gmail.com>
2016-01-01 16:02:50 -0800

    updating deps

--- jspm.config.js
+++ jspm.config.js
@@ -17,7 +17,6 @@ System.config({
   },
 
   map: {
-    "apeman-react-image": "npm:apeman-react-image@1.0.1",
     "babel": "npm:babel-core@5.8.34",
     "babel-runtime": "npm:babel-runtime@5.8.34",
     "core-js": "npm:core-js@1.2.6",
@@ -40,9 +39,6 @@ System.config({
     "github:jspm/nodelibs-events@0.1.1": {
       "events": "npm:events@1.0.2"
     },
-    "github:jspm/nodelibs-os@0.1.0": {
-      "os-browserify": "npm:os-browserify@0.1.2"
-    },
     "github:jspm/nodelibs-path@0.1.0": {
       "path-browserify": "npm:path-browserify@0.0.0"
     },
@@ -61,85 +57,12 @@ System.config({
       "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:apeman-react-icon@1.3.0": {
-      "apeman-react-style": "npm:apeman-react-style@2.2.0",
-      "argx": "npm:argx@1.3.1",
-      "async": "npm:async@1.5.0",
-      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
-      "classnames": "npm:classnames@2.2.1",
-      "extend": "npm:extend@3.0.0",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "ionicons": "npm:ionicons@2.0.1",
-      "path": "github:jspm/nodelibs-path@0.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    },
-    "npm:apeman-react-image@1.0.1": {
-      "apeman-react-icon": "npm:apeman-react-icon@1.3.0",
-      "apeman-react-spinner": "npm:apeman-react-spinner@2.0.11",
-      "apeman-react-style": "npm:apeman-react-style@2.2.0",
-      "argx": "npm:argx@1.3.1",
-      "async": "npm:async@1.5.0",
-      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
-      "classnames": "npm:classnames@2.2.1",
-      "extend": "npm:extend@3.0.0",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "numcal": "npm:numcal@1.1.5",
-      "os": "github:jspm/nodelibs-os@0.1.0",
-      "path": "github:jspm/nodelibs-path@0.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    },
-    "npm:apeman-react-spinner@2.0.11": {
-      "apeman-react-style": "npm:apeman-react-style@2.2.0",
-      "argx": "npm:argx@1.3.1",
-      "async": "npm:async@1.5.0",
-      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
-      "classnames": "npm:classnames@2.2.1",
-      "extend": "npm:extend@3.0.0",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "numcal": "npm:numcal@1.1.5",
-      "os": "github:jspm/nodelibs-os@0.1.0",
-      "path": "github:jspm/nodelibs-path@0.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "react": "npm:react@0.14.5",
-      "react-dom": "npm:react-dom@0.14.5",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    },
-    "npm:apeman-react-style@2.2.0": {
-      "argx": "npm:argx@1.3.1",
-      "arrayfilter": "npm:arrayfilter@1.1.2",
-      "async": "npm:async@1.5.0",
-      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
-      "classnames": "npm:classnames@2.2.1",
-      "extend": "npm:extend@3.0.0",
-      "iftype": "npm:iftype@1.1.1",
-      "onecolor": "npm:onecolor@2.5.0",
-      "os": "github:jspm/nodelibs-os@0.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "react": "npm:react@0.14.5",
-      "react-dom": "npm:react-dom@0.14.5"
-    },
-    "npm:argx@1.3.1": {
-      "iftype": "npm:iftype@1.1.1",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    },
-    "npm:arrayfilter@1.1.2": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:arrayreduce@1.1.0": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
     "npm:asap@2.0.3": {
       "domain": "github:jspm/nodelibs-domain@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
-    },
-    "npm:async@1.5.0": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:babel-runtime@5.8.34": {
       "process": "github:jspm/nodelibs-process@0.1.2"
@@ -195,12 +118,6 @@ System.config({
       "query-string": "npm:query-string@3.0.0",
       "warning": "npm:warning@2.1.0"
     },
-    "npm:iftype@1.1.1": {
-      "argx": "npm:argx@1.3.1",
-      "arrayreduce": "npm:arrayreduce@1.1.0",
-      "async": "npm:async@1.5.0",
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
@@ -224,13 +141,6 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2",
       "stream": "github:jspm/nodelibs-stream@0.1.0",
       "util": "github:jspm/nodelibs-util@0.1.0"
-    },
-    "npm:numcal@1.1.5": {
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    },
-    "npm:os-browserify@0.1.2": {
-      "os": "github:jspm/nodelibs-os@0.1.0"
     },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
