379f0c044f7768f84f7fb6ab34a710fb9b37e9ed
tyrsius <tyrsius@gmail.com>
2016-02-03 16:24:40 -0800

    convert projects to markdown

---
+++ src/projects/affinity-web.md
@@ -0,0 +1,7 @@
+# Affinity Web
+
+The web version of Affinity was a limited scope MVC3 application designed to allow the company's nationwide field technicians to update their work orders on site. Previously they were required to call in to customer support to do so. I had never done web development before starting this project, but my managers were happy to let me learn as I went. I still consider it one of my best accomplishments that the initial version of the application that allowed the techs to create tickets and add notes was live in only six weeks.
+
+The application made use of MVC3 for initial server-side rendering, and jQuery for DOM manipulation and ajax requests. I used tinySort for filtering and ordering ticktet lists and queries, and jqTree for async lazy-loading of what would otherwise be the 10k node category list. The application was very lightweight overall, and had an average page-load time of less than a second. Even on mobile phones, the application was very responsive.
+
+After the initial release, the web application was expanded to give reporting capabilities to our clients. It allowed them to look at an organized view of the open tickets, allowed for multiple format exporting, and gave client admins the abiity to escalate the priority of tickets that had been dispatched to technicians. Unfortunately the parts ordering functionality was not completed before I left the project.
---
+++ src/projects/manifest.js
@@ -0,0 +1,128 @@
+ import JsTestLite from 'projects/jsTestLite.md!text';
+import Swc from 'projects/swc.md!text'
+import AffinityWeb from 'projects/affinity-web.md!text'
+import Affinity from 'projects/affinity.md!text'
+import ChaosCrusade from 'projects/chaos-crusade.md!text';
+import DurandalGrid from 'projects/durandal-grid.md!text'
+import Esp from 'projects/esp.md!text'
+import MasteringKnockout from 'projects/mastering-knockout.md!text'
+import NwMaico from 'projects/nw-maico.md!text'
+import Portfolio from 'projects/portfolio.md!text'
+import Sprintr from 'projects/sprintr.md!text'
+
+
+const manifest = {
+	projects: [
+		{
+			route: 'jsTestLite',
+			title: 'JS Test Lite',
+			type: 'personal',
+			images: [
+				{ src: 'jstest.png', title: 'Write and run tests instantly'}
+			],
+			content: JsTestLite
+		},
+		{
+			route: 'chaos-crusade', 
+			title: 'Chaos Crusade',
+			type: 'personal',
+			images: [
+				{ src: 'chaos_char.jpg', title: 'The character screen, adding a skill'},
+				{ src: 'chaos_skill.jpg', title: 'The skill entry interface.'}
+			],
+			content: ChaosCrusade
+		},
+		{
+			route: 'durandal-grid', 
+			title: 'Durandal Grid',
+			type: 'personal',
+			images: [
+				{ src: 'durandal-grid-basic.png', title: 'The standard grid styling, with paging buttons.'},
+				{ src: 'durandal-grid-custom.png', title: 'Customized rows, with buttons bound to the host viewmodel.'}
+			],
+			content: DurandalGrid
+		},
+		{
+			route: 'portfolio', 
+			title: 'Portfolio',
+			type: 'personal',
+			images: [
+				{ src: 'portfolio_deploy.jpg', title: 'Git deploy: push to the host which unpacks the repo, runs npm build, and starts Forever.'}
+			],
+			content: Portfolio
+		},
+		{
+			route: 'sprintr', 
+			title: 'Sprintr',
+			type: 'personal',
+			images: [
+				{ src: 'sprintr_collapse.jpg', title: 'Sprint with stories collapsed'},
+				{ src: 'sprintr_expand.jpg', title: 'Sprint with stories expanded'}
+			],
+			content: Sprintr
+		},
+		{
+			route: 'nw-maico', 
+			title: 'NwMaico',
+			type: 'professional',
+			images: [
+				{ src: 'nw_home.jpg', title: 'he home page'},
+				{ src: 'nw_parts.jpg', title: 'One of the parts categories'}
+			],
+			content: NwMaico
+		},
+		{
+			route: 'swc', 
+			title: 'ShiftWise Connect',
+			type: 'professional',
+			images: [
+				{ src: 'swc_grid.jpg', title: 'The jobs grid' },
+				{ src: 'swc_user.jpg', title: 'The user options page' },
+				{ src: 'swc_mobile.jpg', title: 'The job details page for mobile browsers' }
+			],
+			content: Swc
+		},
+		{
+			route: 'affinity-web', 
+			title: 'Affinity Web',
+			type: 'professional',
+			images: [
+				{ src: 'web_home.jpg', title: 'The Technicians homepage'},
+				{ src: 'web_ticket.jpg', title: 'he ticket inteface'},
+				{ src: 'web_rtm.jpg', title: 'The customer reporting tool'}
+			],
+			content: AffinityWeb
+		},
+		{
+			route: 'affinity', 
+			title: 'Affinity',
+			type: 'professional',
+			images: [
+				{ src: 'affinity_ticket.jpg', title: 'The Ticket Inteface'},
+				{ src: 'affinity_category.jpg', title: 'The Ticket Inteface'},
+				{ src: 'affinity_parts.jpg', title: 'The Parts Inteface'}
+			],
+			content: Affinity
+		},
+		{
+			route: 'esp', 
+			title: 'Esp',
+			type: 'professional',
+			images: [
+				{ src: '', title: ''},
+			],
+			content: Esp
+		},
+		{
+			route: 'mastering-knockout', 
+			title: 'Mastering Knockout',
+			type: 'publications',
+			images: [
+				{ src: 'mastering_knockout_cover.jpg', title: 'Mastering Knockout', href: 'https://www.packtpub.com/web-development/mastering-knockoutjs'},
+			],
+			content: MasteringKnockout
+		},
+	]
+}
+
+export default manifest
