0e94ed7f8b329a0b3f66d90d9f2d964474ef04d3
Tim Kye <tyrsius@gmail.com>
2018-02-12 18:39:35 -0800

    stonecutter working

--- src/home/Projects.js
+++ src/home/Projects.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
-import { TransitionMotion, spring, presets } from 'react-motion'
+import { presets } from 'react-motion'
+
+import {
+  SpringGrid,
+  measureItems,
+  makeResponsive,
+  layout
+} from 'react-stonecutter'
 
 import { Toggle } from '../components/index.js'
 
 import './projects.css'
+
+const Grid = makeResponsive(measureItems(SpringGrid), {
+  maxWidth: 1920,
+  minPadding: 100
+})
 
 const getProjectTags = projects =>
   Array.from(
@@ -24,62 +36,14 @@ export default class Projects extends Component {
     }
   }
 
-  // actual animation-related logic
-  getDefaultStyles = () => {
-    return this.state.projects.map(project => ({
-      data: project,
-      key: project.name,
-      style: { height: 0, width: 0, opacity: 1 }
-    }))
-  }
-
-  getStyles = () => {
-    const { projects, filter, selectedProject } = this.state
-    return projects
-      .filter(
-        p =>
-          selectedProject
-            ? selectedProject === p.name
-            : filter === 'All' || p.tags.includes(filter)
-      )
-      .map(project => {
-        return {
-          data: project,
-          key: project.name,
-          style:
-            project.name === selectedProject
-              ? {
-                  height: spring(150, presets.stiff),
-                  width: spring(500, presets.stiff),
-                  opacity: spring(1, presets.stiff)
-                }
-              : {
-                  height: spring(60, presets.stiff),
-                  width: spring(100, presets.stiff),
-                  opacity: spring(1, presets.stiff)
-                }
-        }
-      })
-  }
-
-  willEnter() {
-    return {
-      height: 0,
-      width: 0,
-      opacity: 1
-    }
-  }
-
-  willLeave() {
-    return {
-      height: spring(0),
-      width: spring(0),
-      opacity: spring(0)
-    }
-  }
-
   render() {
     let { filter, projects, tags, selectedProject } = this.state
+    projects = projects.filter(
+      p =>
+        selectedProject
+          ? selectedProject === p.name
+          : filter === 'All' || p.tags.includes(filter)
+    )
     return (
       <div className="projects-container">
         {!selectedProject ? (
@@ -94,34 +58,26 @@ export default class Projects extends Component {
             Return to Projects
           </button>
         )}
-        <TransitionMotion
-          defaultStyles={this.getDefaultStyles()}
-          styles={this.getStyles()}
-          willLeave={this.willLeave}
-          willEnter={this.willEnter}
+        <Grid
+          springConfig={presets.gentle}
+          layout={layout.simple}
+          columnWidth={150}
+          gutterWidth={5}
+          gutterHeight={5}
         >
-          {styles => (
-            <div className="projects">
-              {styles.map(({ key, style, data: project }) => {
-                return (
-                  <div
-                    key={key}
-                    style={style}
-                    onClick={() =>
-                      this.setState({ selectedProject: project.name })
-                    }
-                  >
-                    {project.name === selectedProject ? (
-                      <Project project={project} />
-                    ) : (
-                      <ProjectTile project={project} />
-                    )}
-                  </div>
-                )
-              })}
+          {projects.map(project => (
+            <div
+              key={project.name}
+              onClick={() => this.setState({ selectedProject: project.name })}
+            >
+              {project.name === selectedProject ? (
+                <Project project={project} />
+              ) : (
+                <ProjectTile project={project} />
+              )}
             </div>
-          )}
-        </TransitionMotion>
+          ))}
+        </Grid>
       </div>
     )
   }
@@ -130,7 +86,7 @@ export default class Projects extends Component {
 const Project = ({ project: { title, icon, body } }) => (
   <div className="project open">
     {icon}
-    {title}
+    <h3>{title}</h3>
     {body}
   </div>
 )
@@ -172,54 +128,54 @@ const projectManifest = [
     name: 'rocket',
     title: 'Rocket',
     tags: ['Professional', 'Front End'],
-    icon: <img src="/images/projects/rocket.svg" />
+    icon: <img alt="" src="/images/projects/rocket.svg" />
   },
   {
     name: 'devportal',
     title: 'Niketech Developer Portal',
     tags: ['Security', 'Professional', 'Front End'],
-    icon: <img src="/images/projects/code_icon.svg" />
+    icon: <img alt="" src="/images/projects/code_icon.svg" />
   },
   {
     name: 'auth-toolkit',
     title: 'NGP Auth Toolkit',
     tags: ['Open Source', 'Security'],
-    icon: <img src="/images/projects/oauth_logo.svg" />
+    icon: <img alt="" src="/images/projects/oauth_logo.svg" />
   },
   {
     name: 'grey-market',
     title: 'Grey Market',
     tags: ['Professional', 'Front End'],
-    icon: <img src="/images/projects/market_icon.svg" />
+    icon: <img alt="" src="/images/projects/market_icon.svg" />
   },
   {
     name: 'cryonic',
     title: 'Cryonic',
     tags: ['Performance', 'Professional'],
-    icon: <img src="/images/projects/snow.svg" />
+    icon: <img alt="" src="/images/projects/snow.svg" />
   },
   {
     name: 'dabber',
     title: 'dabber',
     tags: ['Open Source'],
-    icon: <img src="/images/projects/dynamodb.svg" />
+    icon: <img alt="" src="/images/projects/dynamodb.svg" />
   },
   {
     name: 'aws-thin-libraries',
     title: 'AWS Thin Libraries',
     tags: ['Performance', 'Open Source'],
-    icon: <img src="/images/projects/aws.svg" />
+    icon: <img alt="" src="/images/projects/aws.svg" />
   },
   {
     name: 'mastering-knockoutjs',
     title: 'Mastering Knockout JS',
     tags: ['Publications'],
-    icon: <img src="/images/projects/mastering_knockout_cover.jpg" />
+    icon: <img alt="" src="/images/projects/mastering_knockout_cover.jpg" />
   },
   {
     name: 't-plus-plus',
     title: 'T++ Blog',
     tags: ['Publications'],
-    icon: <img src="/images/projects/blog_logo.jpg" />
+    icon: <img alt="" src="/images/projects/blog_logo.jpg" />
   }
 ]
